a02b949ed0d47a2e5bf7231094cb91a4327b8ab9
wesleymirada <wymirandac@gmail.com>
2019-09-05 00:00:00 -0400

    Iniciando o detector de expressoes

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <dlib/opencv.h>
+#include <dlib/image_processing.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+
+using namespace cv;
+using namespace dlib;
+
+int main() {
+
+
+	std::cin.get();
+
+	return 0;
+}
