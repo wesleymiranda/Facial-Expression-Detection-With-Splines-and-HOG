717bf74f1425c769b53993c69eccc0a5fdb24d95
wesleymirada <wymirandac@gmail.com>
2019-09-14 00:00:00 -0400

    Iniciando o detector de expressoes

--- src/splines.cpp
+++ src/splines.cpp
@@ -27,3 +27,27 @@ void spline::setPointCoord(Eigen::VectorXd x_, Eigen::VectorXd y_) {
 	y = y_;
 }
 
+void spline::setDifferenceH() {
+	double interval =(double) 1 / N;
+	h = Eigen::VectorXd::Constant(N, 1, interval);
+}
+
+void spline::setMatrixA() {
+	A = Eigen::MatrixXd::Zero(N - 1, N - 1);
+
+	for (uint64_t i = 0; i < N - 1; i++) {
+		if (i > 0) A(i, i - 1) = h(i);
+		A(i, i) = 2 * (h(i + 1) + h(i));
+		if (i < N - 2) A(i, i + 1) = h(i + 1);
+	}
+}
+
+void spline::setVectorB() {
+	B_x = Eigen::VectorXd::Zero(N - 1);
+	B_y = Eigen::VectorXd::Zero(N - 1);
+
+	for (uint64_t i = 1; i < N; i++) {
+		B_x(i - 1) = 6 * ((x(i + 1) - x(i)) / h(i) - (x(i) - x(i - 1)) / h(i - 1));
+		B_y(i - 1) = 6 * ((y(i + 1) - y(i)) / h(i) - (y(i) - y(i - 1)) / h(i - 1));
+	}
+}
