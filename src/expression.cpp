a7595cbeddac9ab01cc445061fc88ba31d2addad
wesleymirada <wymirandac@gmail.com>
2019-10-02 00:00:00 -0400

    Iniciando o detector de expressoes

--- src/expression.cpp
+++ src/expression.cpp
@@ -27,3 +27,25 @@ expression::expression(cv::Mat &image, full_object_detection faceLandmarks_) {
 void expression::setLandmarks(full_object_detection faceLandmarks_) {
 	faceLandmarks = faceLandmarks_;
 }
+
+void expression::setPoints(int init, int size, spline &s, int other = 0) {
+	Eigen::VectorXd x;
+	Eigen::VectorXd y;
+	x = Eigen::VectorXd::Zero(size);
+	y = Eigen::VectorXd::Zero(size);
+
+	for (int i = 0; i < size; i++) {
+		x(i) =  (double)faceLandmarks.part(i + init).x();
+		y(i) =  (double)faceLandmarks.part(i + init).y();
+	}
+
+	if (other != 0) {
+		x.conservativeResize(x.size() + 1);
+		y.conservativeResize(y.size() + 1);
+
+		x(x.size() - 1) =  (double)faceLandmarks.part(other).x();
+		y(y.size() - 1) =  (double)faceLandmarks.part(other).y();
+	}
+
+	s = spline(x, y);
+}
